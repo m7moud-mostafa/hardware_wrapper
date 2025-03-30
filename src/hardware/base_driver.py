b76b53440ad704a240a2129f4a0ed1f1d87e3503
MahmoudOnUbuntuDeskTop <mah2002moud@gmail.com>
2025-03-30 16:00:58 +0200

    Ros1 base strudcture

---
+++ src/hardware/base_driver.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""This script contains the BaseDriver class"""
+
+class BaseDriver:
+    """
+    BaseDriver class will include the necessary functions
+    of any driver
+    """
+
+
+    def __init__(self):
+        """Initializing the function"""
+        pass
+
+    def receive(self):
+        pass
+
+    def send(self):
+        pass
+
+    def logger(self):
+        pass
+
+    def __str__(self):
+        pass
