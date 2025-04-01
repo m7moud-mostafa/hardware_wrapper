9d51dd5ca73360fbb13a23c58216dba4592ed59e
MahmoudOnUbuntuDeskTop <mah2002moud@gmail.com>
2025-04-01 06:07:50 +0200

    Implemented the CAN Driver Receive/Send, Not tested yet

--- src/hardware/can_driver.py
+++ src/hardware/can_driver.py
@@ -1,1 +1,93 @@
+#!/usr/bin/python3
+"""
+CANReceiver and CANSender classes to handle hardware communication
+
+Author: Mahmoud Mostafa
+Email: mah2002moud@gmail.com
+"""
+
 import can
+from hardware.base_driver import BaseDriver
+
+class CANBaseDriver(BaseDriver):
+    """General CAN base driver containing common functionalities"""
+
+    def __init__(self, msgName, operation, msgID, extendedID=False, channel="can0", bitrate=250000):
+        super().__init__(msgName, operation, msgID)
+        self.channel = channel
+        self.extendedID = extendedID
+        self.bitrate = bitrate
+
+    @property
+    def bitrate(self):
+        return self.__bitrate
+
+    @bitrate.setter
+    def bitrate(self, value):
+        if not isinstance(value, int):
+            raise TypeError("'bitrate' value must be of type (int)")
+        self.__bitrate = value
+
+    @property
+    def channel(self):
+        return self.__channel
+
+    @channel.setter
+    def channel(self, value):
+        if not isinstance(value, str):
+            raise TypeError("'channel' value must be of type (str)")
+        self.__channel = value
+
+    @property
+    def extendedID(self):
+        return self.__extendedID
+
+    @extendedID.setter
+    def extendedID(self, value):
+        if not isinstance(value, bool):
+            raise TypeError("extendedID value must be of type (bool)")
+        self.__extendedID = value
+
+class CANSender(CANBaseDriver):
+    """CANSender class handles sending messages through CAN"""
+
+    def __init__(self, msgName, msgID, extendedID=False, channel="can0", bitrate=250000):
+        super().__init__(msgName, "send", msgID, extendedID, channel, bitrate)
+        self.bus = can.interface.Bus(channel=self.channel, bustype='socketcan', bitrate=self.bitrate)
+
+    def send(self, data):
+        """Send a CAN message"""
+        try:
+            msg = can.Message(arbitration_id=self.msgID, data=data, is_extended_id=self.extendedID)
+            self.bus.send(msg)
+            print(f"Sent CAN message: ID={self.msgID}, Data={data}")
+        except can.CanError as e:
+            print(f"Failed to send CAN message: {e}")
+
+    def receive(self):
+        raise NotImplementedError("'CANSender' object can't be used to receive messages")
+
+class CANReceiver(CANBaseDriver):
+    """CANReceiver class handles receiving messages through CAN"""
+
+    def __init__(self, msgName, msgID, extendedID=False, channel="can0", bitrate=250000):
+        super().__init__(msgName, "receive", msgID, extendedID, channel, bitrate)
+        self.__filter = [{
+            "can_id": self.msgID, 
+            "can_mask": 0x7FF if not self.extendedID else 0x1FFFFFFF, 
+            "extended": self.extendedID
+        }]
+        self.bus = can.interface.Bus(channel=self.channel, bustype='socketcan', bitrate=self.bitrate, can_filters=self.__filter)
+
+    def receive(self):
+        """Receive a CAN message"""
+        msg = self.bus.recv(timeout=5.0)  # will wait 5 seconds for the messages before returning None
+        if msg:
+            print(f"Received CAN message: ID={msg.arbitration_id}, Data={msg.data}")
+            return msg
+        else:
+            print("No CAN message received within timeout.")
+            return None
+
+    def send(self):
+        raise NotImplementedError("'CANReceiver' object can't be used to send messages")
